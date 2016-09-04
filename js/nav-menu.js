787fbbea13a6edece975f6c1b1b4b22b218f289a
Levona Yim <P206073@WS5750.local>
2016-09-04 15:09:14 -0700

    fix(home):removed automatic menu popup
    added new font
    
    fixing grid system
    
    adjust breakpoint for project detail page

--- js/nav-menu.js
+++ js/nav-menu.js
@@ -1,22 +1,20 @@
 $(document).ready( function() {
 	$("a.dd-toggle").click( function(e) { 
-		
+	
 		e.preventDefault();
-		
-		if ( $(this).next("h5").hasClass("menu-hidden") ) {
-			$(this).next("h5").removeClass("menu-hidden").addClass("menu-shown");
-		}
-		else {
-			$(this).removeClass("menu-shown").addClass("menu-hidden");
-		}		
+	
 		
 		if ( $(this).next("ul").hasClass("dd-shown") ) {
 			$(this).next("ul").removeClass("dd-shown").addClass("dd-hidden");
+			$(this).html('Menu').show().siblings('dd-shown').hide();
 		} 
+
 		else {
 			$(this).next("ul").removeClass("dd-hidden").addClass("dd-shown");
+			$(this).html('Close').show().siblings('dd-hidden').hide();
 		}
 	});
+
 
 
 });
